cfaf8f3ee3a3f709f9721efa24be589404a9a92e
BenjaminAston <aston.benjamin@gmail.com>
2024-04-08 09:43:52 +0200

    Update bottle.cpython-312.pyc

--- Python för healhub/python.py
+++ Python för healhub/python.py
@@ -1,10 +1,10 @@
 from bottle import route, run, template, redirect, static_file, request
 import json
 
-# Function to read data from "wiki.json"
+#Function to read data from "wiki.json"
 def get_info_from_file():
     try:
-        with open("static/wiki.json", "r") as my_file:
+        with open("python för healhub/static/wiki.json", "r") as my_file:
             info = json.load(my_file)
         return info
     except FileNotFoundError:
@@ -12,7 +12,7 @@ def get_info_from_file():
             json.dump([], my_file)
         return []
 
-# Function to generate a unique ID for new entries
+#Function to generate a unique ID for new entries
 def create_id(info):
     highest_id = 1
     for entry in info:
@@ -20,24 +20,23 @@ def create_id(info):
             highest_id = entry["id"] + 1
     return highest_id
 
-# Route to serve the index page
+#Route to serve the index page
 @route("/")
 def index():
     info = get_info_from_file()
-    return template("index", info=info)
+    return template("python för healhub/views/index.html", info=info)
 
-# Route to serve static CSS files
-@route("/static/css/<filename>")
+#Route to serve static CSS files
+@route("/python för healhub/static/css/<filename>")
 def serve_css(filename):
-    return static_file(filename, root="static/css")
+    return static_file(filename, root="python för healhub/static/css")
 
-
-# Additional route to serve the second CSS file (style3.css)
-@route("/static/css/style3.css")
+#Additional route to serve the second CSS file (style3.css)
+@route("/python för healhub/static/css/style3.css")
 def serve_css3():
-    return static_file("style3.css", root="static/css")
+    return static_file("style3.css", root="python för healhub/static/css")
 
-# Route to handle saving new entries
+#Route to handle saving new entries
 @route("/save/", method="POST")
 def save_info():
     title = request.forms.get("title")
@@ -57,6 +56,6 @@ def save_info():
 
     redirect("/")
 
-# Run the server
+#Run the server
 if __name__ == "__main__":
     run(host="127.0.0.1", port=8080, debug=True)
