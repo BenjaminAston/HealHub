5cf8ba2b0651a4f7ddf89a6ed569fe8166ee6f45
Andsansa <164369283+Andsansa@users.noreply.github.com>
2024-06-02 12:07:02 +0200

    feedback uppdatering

--- HealHub_code/static/framsteg.js
+++ HealHub_code/static/framsteg.js
@@ -5,5 +5,6 @@ document.getElementById('feedback-form').onsubmit = function(event) {
 
     document.getElementById('feedback-dialog').innerHTML = `
         <h3>Tack för din feedback!</h3>
-        <button onclick="document.getElementById('feedback-dialog').close()">Stäng</button>`;
+        <button onclick="document.getElementById('feedback-dialog').close()">Stäng</button>
+        `;
 }
